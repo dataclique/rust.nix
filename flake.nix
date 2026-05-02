110ea91a4078258b13604519761731438e456876
0xgleb <gleb.dianov@protonmail.com>
2026-05-02 16:44:57 +0700

    clean up fmt

--- flake.nix
+++ flake.nix
@@ -2,24 +2,29 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
-    git-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    devenv = {
-      url = "github:cachix/devenv";
-      inputs.git-hooks.follows = "git-hooks";
-    };
 
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
+
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.git-hooks.follows = "git-hooks";
+
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    { self, nixpkgs, flake-utils, devenv, git-hooks, fenix, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      devenv,
+      git-hooks,
+      fenix,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
@@ -30,32 +35,33 @@
         hooks = {
           actionlint.enable = true;
           taplo.enable = true;
-          nixfmt-classic.enable = true;
+          nixfmt.enable = true;
+
           rustfmt = {
             enable = true;
             packageOverrides = { inherit (toolchain) cargo rustfmt; };
           };
         };
 
-      in {
+      in
+      {
         devShells = {
           default = devenv.lib.mkShell {
             inherit inputs pkgs;
-            modules = [{
-              # https://devenv.sh/reference/options/
-              packages = with pkgs;
-                [ cargo-watch ] ++ lib.optionals stdenv.isDarwin [ libiconv ];
+            modules = [
+              {
+                # https://devenv.sh/reference/options/
+                packages = with pkgs; lib.optionals stdenv.isDarwin [ libiconv ];
 
-              env.LOG_LEVEL = "DEBUG";
+                languages.rust = {
+                  enable = true;
+                  inherit toolchain;
+                };
 
-              languages.rust = {
-                enable = true;
-                inherit toolchain;
-              };
-
-              difftastic.enable = true;
-              git-hooks = { inherit hooks; };
-            }];
+                difftastic.enable = true;
+                git-hooks = { inherit hooks; };
+              }
+            ];
           };
         };
 
@@ -85,47 +91,47 @@
             touch $out
           '';
         };
-      }) // {
-        templates = {
-          default = self.templates.rust;
+      }
+    )
+    // {
+      templates = {
+        default = self.templates.rust;
 
-          rust = {
-            path = ./.;
-            description =
-              "Rust dev shell (devenv + fenix + pre-commit) with CI";
-            welcomeText = ''
-              # Rust + Nix template
+        rust = {
+          path = ./.;
+          description = "Rust dev shell (devenv + fenix + pre-commit) with CI";
+          welcomeText = ''
+            # Rust + Nix template
 
-              Next steps:
-                1. `direnv allow` (or `nix develop --impure`) to enter the dev shell.
-                2. `cargo run` to verify the toolchain.
-                3. Edit `Cargo.toml` to set your crate name.
+            Next steps:
+              1. `direnv allow` (or `nix develop --impure`) to enter the dev shell.
+              2. `cargo run` to verify the toolchain.
+              3. Edit `Cargo.toml` to set your crate name.
 
-              Optional cleanup: this flake inherits a `templates` output
-              that re-exposes the project as a sub-template. If you don't
-              plan to re-expose templates from your project, you can
-              delete the `templates` block from `flake.nix`. It's
-              otherwise harmless.
-            '';
-          };
+            Optional cleanup: this flake inherits a `templates` output
+            that re-exposes the project as a sub-template. If you don't
+            plan to re-expose templates from your project, you can
+            delete the `templates` block from `flake.nix`. It's
+            otherwise harmless.
+          '';
+        };
 
-          ci = {
-            path = ./templates/ci;
-            description = "GitHub Actions CI for a Nix-flake Rust project";
-            welcomeText = ''
-              # CI-only template
+        ci = {
+          path = ./templates/ci;
+          description = "GitHub Actions CI for a Nix-flake Rust project";
+          welcomeText = ''
+            # CI-only template
 
-              Drops `.github/workflows/ci.yaml` into your project. Assumes
-              your flake exposes a `devShells.default` that provides
-              `cargo` and `clippy`.
-            '';
-          };
+            Drops `.github/workflows/ci.yaml` into your project. Assumes
+            your flake exposes a `devShells.default` that provides
+            `cargo` and `clippy`.
+          '';
         };
       };
+    };
 
   nixConfig = {
-    extra-trusted-public-keys =
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     extra-substituters = "https://devenv.cachix.org";
   };
 }
